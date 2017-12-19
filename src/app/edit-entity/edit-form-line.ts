a4c373143c1c81e48952bca18a3592e07e29ef92
Rodrigo Vilar <rodrigovilar@dcx.ufpb.br>
2017-12-18 23:27:30 -0300

    Removing two way data binding

--- src/app/edit-entity/edit-form-line.ts
+++ src/app/edit-entity/edit-form-line.ts
@@ -5,15 +5,16 @@ import { PropertyComponent } from 'angularm';
 @Component({
     selector: 'div [mgEditFormLine]',
     template:
-        `<label
+        `<div [formGroup]="mgFormControl.parent">
+        <label
             for="{{property.propertyType.entityType.singular}}_{{property.propertyType.name}}"
         >{{property.propertyType.name | titleCase}}</label>
         <input
             type="{{configuration.inputType}}"
             id="{{property.propertyType.entityType.singular}}_{{property.propertyType.name}}"
             placeholder="{{property.propertyType.name | titleCase}}"
-            [formControl]="mgFormControl"
-            [(ngModel)]="property.value">
-        <br>`,
+            formControlName="{{property.propertyType.name}}">
+        <br>
+        </div>`,
 })
 export class EditFormLineComponent extends PropertyComponent { }
